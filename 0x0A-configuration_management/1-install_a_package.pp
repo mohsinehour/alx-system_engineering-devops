906f0c8a0f1385d222e55ee2e3215e167614340f
Mohsine Hourmat Allah <125808498+mohsinehour@users.noreply.github.com>
2024-04-19 17:49:28 +0100

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
-#This puppet mainfest installs flask from pip3
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
